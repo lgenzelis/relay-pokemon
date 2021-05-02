f5dc0d3a79411c139ee9c624d18f33b5748a9527
Lucas Genzelis <lucas@foxbox.com>
2021-05-01 21:26:28 -0300

    relay initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,50 @@
+// your-app-name/src/App.js
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import fetchGraphQL from './fetchGraphQl';
+
+const { useState, useEffect } = React;
 
 function App() {
+  // We'll load the name of a repository, initially setting it to null
+  const [name, setName] = useState(null);
+
+  // When the component mounts we'll fetch a repository name
+  useEffect(() => {
+    let isMounted = true;
+    fetchGraphQL(`
+      query RepositoryNameQuery {
+        # wow, so cool 🤩
+        repository(owner: "lgenzelis" name: "next-barebone") {
+          name
+        }
+      }
+    `).then(response => {
+      if (response.errors) {
+        throw new Error(JSON.stringify(response.errors));
+      }
+      // Avoid updating state if the component unmounted before the fetch completes
+      if (!isMounted) {
+        return;
+      }
+      const data = response.data;
+      setName(data.repository.name);
+    }).catch(error => {
+      console.error(error);
+    });
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  // Render "Loading" until the query completes
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {name != null ? `Repository: ${name}` : "Loading"}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
