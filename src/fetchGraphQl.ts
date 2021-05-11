ef4525e89098c3c99c99cc04c700ef4a0491c938
Lucas Genzelis <lucas.genzelis@gmail.com>
2021-05-11 13:37:37 -0300

    chore: rename fetchGraphQl params

--- src/fetchGraphQl.ts
+++ src/fetchGraphQl.ts
@@ -1,13 +1,10 @@
-async function fetchGraphQL(text: string, variables?: Record<string, any>) {
+async function fetchGraphQL(query: string, variables?: Record<string, any>) {
   const response = await fetch('https://graphql-pokemon2.vercel.app/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({
-      query: text,
-      variables,
-    }),
+    body: JSON.stringify({ query, variables }),
   });
 
   return await response.json();
